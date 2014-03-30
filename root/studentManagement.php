a329568d7ea2fb80a227022a15598be31dee32f0
Chris Gwozdecky <cgwozdecky@gmail.com>
2014-03-30 17:35:33 -0400

    Revert "initial changes"
    This reverts commit 6bd959339a616e4d7bef0c310cc30b7541a9bacf.

--- root/studentManagement.php
+++ root/studentManagement.php
@@ -80,24 +80,6 @@
         <input name="ViewStudent" type="button" value="Delete Student" style="width:140px"/>
       </p>
     </div>
-     <?php
-if (version_compare(PHP_VERSION, '5.3.7', '<')) {
-    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
-} else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
-    require_once("libraries/password_compatibility_library.php");
-}
-
-require_once("config/db.php");
-require_once("classes/StudentManagement.php");
-
-$student = new Student();
-if ($login->isUserLoggedIn() == true) {
-    include("views/studentManagement.php");  
-} else {
-    include("views/not_logged_in.php");
-}
-
-?>
   </div>
   <!--close container-->
   
