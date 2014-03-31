d9b527e8bd84fc371ec32ec59793a7b78811f657
Mtoderian <Mtoderian@gmail.com>
2014-03-31 11:24:27 -0400

    slight change to student management page
    added php protocol, code incorrect currently.

--- root/studentManagement.php
+++ root/studentManagement.php
@@ -79,6 +79,24 @@
         <input name="ViewStudent" type="button" value="Save Student" style="width:140px"/>
         <input name="ViewStudent" type="button" value="Delete Student" style="width:140px"/>
       </p>
+       <?php
+if (version_compare(PHP_VERSION, '5.3.7', '<')) {
+    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+} else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+    require_once("libraries/password_compatibility_library.php");
+}
+
+require_once("config/db.php");
+require_once("classes/Student.php");
+
+$login = new Login();
+if ($login->isUserLoggedIn() == true) {
+    include("views/teacherWelcome.php");  
+} else {
+    include("views/not_logged_in.php");
+}
+
+?>
     </div>
   </div>
   <!--close container-->
